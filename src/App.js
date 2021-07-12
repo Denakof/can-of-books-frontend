bcaddca0cb0008839249b8b33f721caf9c1081d4
denakof <denakofahi_21@yahoo.com>
2021-07-12 23:49:35 +0300

    lab12-front

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import User from './User'
-import Profile from './Profile'
+import React from "react";
+import Header from "./Header";
+import IsLoadingAndError from "./IsLoadingAndError";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import User from "./User";
+import Profile from "./Profile";
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props);
-    return(
+    console.log("app", this.props);
+    return (
       <>
         <Router>
-          <IsLoadingAndError>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-                <User/>
-                </Route>
-              <Route exact path="/Profile"> <Profile/> </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
-          </IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              <User />
+            </Route>
+            <Route exact path="/Profile">
+              {" "}
+              <Profile />{" "}
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+          </Switch>
+          <Footer />
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,17 +2,61 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import axios from 'axios';
+import { Card } from 'react-bootstrap';
+import { withAuth0 } from "@auth0/auth0-react";
 
 class MyFavoriteBooks extends React.Component {
+
+constructor(props){
+  super(props)
+
+  this.state = {
+    book:null,
+     
+  };
+};
+
+componentDidMount(){
+  axios
+    .get(`http://localhost:3002/books?email=${this.props.auth0.user.email}`)
+    .then((results) => {
+      console.log(results.length);
+      this.setState({
+        book: results.data.map((element) => {
+          return (
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                style={{ width: "18rem" }}
+                variant="top"
+                src={element.img}
+              />
+              <Card.Body>
+                <Card.Title>{element.name}</Card.Title>
+                <Card.Text>{element.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          );
+        }),
+      });
+    });
+    .catch((error) => {
+      console.log(error);
+    });
+  }
   render() {
     return(
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
+
+        
         </p>
+        <>{this.state.books}</>
+        
       </Jumbotron>
-    )
+    );
   }
 }
 
